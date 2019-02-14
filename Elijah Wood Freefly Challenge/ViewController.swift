339be713332c835460696607426630b78fe27c30
EW MBP17 <elijah@qello.com>
2019-02-14 11:45:50 -0800

    Initial Commit

---
+++ Elijah Wood Freefly Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Elijah Wood Freefly Challenge
+//
+//  Created by Elijah Wood on 2/14/19.
+//  Copyright © 2019 Elijah Wood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
